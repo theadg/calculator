eb4b46258088989024a4a9dff5fb64a0366d9c0b
theadg <drewndrei@gmail.com>
2022-08-05 23:01:58 +0800

    Add numCont obj, ope, conv , add & equal btn FNs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const numButtons = document.querySelectorAll('.num');
 const calcOutput = document.querySelector(".screen-output");
         calcOutput.textContent = "";
 let number = "";
-
+let initialNum, secondaryNum;
 // putting the number into the calc screen
 // MAKE IT FOR FLOATS AS WELL
 numButtons.forEach((button) =>{
@@ -18,13 +18,17 @@ numButtons.forEach((button) =>{
         // output num 
        
         // number = parseInt(number); //converting number from string to int
-        calcOutput.textContent = addComma(number);
+        calcOutput.textContent = (addComma(number).numberArr);
         }
 
 
     }
 });
 
+let numContainer ={
+    number: 0,
+    numContainer: 0,
+}
 // adding commas
 function addComma(number){ 
     
@@ -41,32 +45,84 @@ function addComma(number){
         }
         
         const numComma = numArr.join('');
-        return numComma;
+        // return numComma;
+        numContainer = {
+            number: number,
+            numberArr: numComma,
+        }   
+        return numContainer;
     } else{
         numComma = number;
-        return numComma;
+       numContainer = {
+            number: number,
+            numberArr: numComma,
+        } 
+        // return numComma;
+        return numContainer;
     }
     
     // console.log(numArr);
 }
 
+
+
+
 //declaration
 const operators = document.querySelectorAll('.operator');
 const calcInput = document.querySelector('.screen-calculations');
 
-operators.forEach((operator) => {
-    operator.onclick = () => {
-        if (calcOutput.textContent === ""){
-            alert("Nothing to compute");
-            clearUpperScreen();
-        } else {
+// operators.forEach((operator) => {
+//     // operator.onclick = () => {
+//     //     if (calcOutput.textContent === ""){
+//     //         alert("Nothing to compute");
+//     //         clearUpperScreen();
+//     //     } else {
 
-        calcInput.textContent = `${calcOutput.textContent} ${operator.textContent}`;
-        number = "";
-        clearBottomScreen();
-        }
+//     //     initialNum = calcOutput.textContent;
+     
+//     //     calcInput.textContent = `${initialNum} ${operator.textContent}`;
+//     //     convert(initialNum);
+//     //     number = "";
+//     //     clearBottomScreen();
+//         }
+//     }
+// });
+
+// converting num 
+convert = (initialNum) => {
+    const numArr  =  Array.from((initialNum));
+    console.log(numArr);
+    const index = numArr.indexOf(",");
+    numArr.splice(index,1);
+    console.log(numArr);
+};
+
+
+//operator
+operate = () => {
+    if (calcOutput.textContent === ""){
+        alert("Nothing to compute");
+        clearUpperScreen();
+    } else {
+
+    initialNum = calcOutput.textContent;
+ 
+    calcInput.textContent = `${initialNum} ${operator}`;
+
+    initialNum = numContainer.number;
+    console.log(initialNum);
+    number = "";
+    clearBottomScreen();
     }
-})
+};
+//getting operator 
+const add = document.querySelector("#add");
+let operator = ""
+
+add.onclick = () =>{
+    operator = "+";
+    operate();
+}
 
 function clearBottomScreen(){
     calcOutput.textContent = "";
@@ -79,8 +135,28 @@ function clearBottomScreen(){
 
  //declaration
  const allClear = document.querySelector('.all-clear');
+ //clearing both screens
  allClear.onclick = () => {
   clearUpperScreen();
   clearBottomScreen();
   number = "";  
+ };
+
+
+ //declaration
+ const equalBtn = document.getElementById("equals");
+
+ equalBtn.onclick = () => {
+    secondaryNum = calcOutput.textContent;
+    compute(initialNum,secondaryNum);
+    
+ }
+
+
+ //computing num
+compute = (initialNum,secondaryNum) =>{
+    if (operator === "+"){
+        output = parseInt(initialNum) + parseInt(secondaryNum);
+        calcOutput.textContent = output;
+    }
  };
